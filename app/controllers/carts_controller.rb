5c43ceb0ea610294d663eb57e477bfcdfb57940b
dany-about <dany.about@edu.escpeurope.eu>
2019-03-05 20:46:42 +0100

    cleaned routes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,29 +3,10 @@ class CartsController < ApplicationController
   after_action :add_product_to_cart_from_product_page, only: [:create]
   respond_to :html, :js
 
-  # GET /carts
-  # GET /carts.json
-  def index
-    @carts = Cart.all
-  end
-
-  # GET /carts/1
-  # GET /carts/1.json
   def show
     @products = Product.all
   end
 
-  # GET /carts/new
-  def new
-    @cart = Cart.new
-  end
-
-  # GET /carts/1/edit
-  def edit
-  end
-
-  # POST /carts
-  # POST /carts.json
   def create
     if current_user.cart == nil
       @cart = Cart.create!(user: current_user)
@@ -33,27 +14,12 @@ class CartsController < ApplicationController
     redirect_to product_path(params[:product_id])
   end
 
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
-
-  # LES NOTICE / FLASH NE MARCHENT PAS. A ADAPTER A AJAX
-
   def update
     @products = Product.all
     if params[:to_do] == "add" then current_user.cart.add_product(Product.find(params[:product_id])) end
     if params[:to_do] == "remove" then current_user.cart.remove_product(Product.find(params[:product_id])) end
     if params[:to_do] == "delete" then current_user.cart.products.where(id: params[:product_id]).count.times {current_user.cart.remove_product(Product.find(params[:product_id]))} end
     redirect_to cart_path(@cart)
-  end
-
-  # DELETE /carts/1
-  # DELETE /carts/1.json
-  def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   def add_product_to_cart_from_product_page
