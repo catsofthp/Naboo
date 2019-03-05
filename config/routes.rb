8a40ff28b2beb930bcaef6ce6696afd2f0a94ec5
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-03-05 12:54:33 +0100

    unable gem mini_racer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :products do
     resources :carts
   end
+  resources :charges
+  
   get 'home/users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
