02aef79426748100c597b606cf77c3757d9d8e49
dany-about <dany.about@edu.escpeurope.eu>
2019-03-06 16:24:14 +0100

    jquery for scroll

--- app/assets/javascripts/orders_scripts.js
+++ app/assets/javascripts/orders_scripts.js
@@ -4,7 +4,23 @@
     $(this).next().toggleClass("collapse");
    })
   
+  function scrollToAnchor(aid){
+    var aTag = $(aid);
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  }
 
+  $("a[href='#deleteAccount']").on("click", function (event) { 
+    event.preventDefault()
+    scrollToAnchor("#deleteAccount")
+  })
+  $("a[href='#showAccount']").on("click", function (event) { 
+    event.preventDefault()
+    scrollToAnchor("#showAccount")
+  })
+  $("a[href='#showOrders']").on("click", function (event) { 
+    event.preventDefault()
+    scrollToAnchor("#showOrders")
+  })
 
 
  })
