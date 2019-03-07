7cafb47d3e03c52cfb7bfb1dc50a1d2a5886d97c
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-03-07 12:34:59 +0100

    routes dry

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
       resources :charges, only: [:new, :create], path: ""
     end
   end
-
-  resources :products do
+  
+  #resources :products do
     resources :avatars, only: [:create]
-  end
+  #end
+
   get 'home/users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
