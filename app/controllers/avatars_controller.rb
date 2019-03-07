56a13b130e48e3a618ff248ce47aa362fc1fc5d8
nans64 <nans.noel@gmail.com>
2019-03-07 23:40:32 +0100

    Fix Images

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,7 +2,7 @@ class AvatarsController < ApplicationController
 
   def create
 
-    @product = Product.find_by(title: params[:product_id].capitalize)
+    @product = Product.friendly.find(params[:product_id])
 
     @product.avatar.attach(params[:avatar])
     redirect_to(product_path(@product))
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,7 +73,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :price, :image_url)
+      params.require(:product).permit(:title, :description, :price, :image_url, :image)
     end
 
     def is_owner?
