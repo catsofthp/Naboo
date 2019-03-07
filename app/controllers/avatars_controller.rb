afaa1d62de941e7b99158ee539903d9787c4103a
dany-about <dany.about@edu.escpeurope.eu>
2019-03-07 11:56:28 +0100

    RAS

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,7 @@
 class AvatarsController < ApplicationController
 
   def create
-    @product = Product.find(params[:product_id])
+    @product = Product.find(Product.find_by(title: params[:product_id].capitalize)
     @product.avatar.attach(params[:avatar])
     redirect_to(product_path(@product))
   end
