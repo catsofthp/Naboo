874c3749e44fb074e10d68208a5c92f48156917a
nans64 <nans.noel@gmail.com>
2019-03-07 12:37:07 +0100

    Email changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,19 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     mail(from: "nans.noel@gmail.com", to: user.email,
-         subject: "This is a nice welcome email")
+         subject: "Bienvenue chez Naboo cat store !")
   end
 
   def welcome_order(order)
     @order = order
-<<<<<<< HEAD
     mail(from: "nans.noel@gmail.com", to: order.user.email,
-=======
-    mail(from: "nans.noel@gmail.com", to: "nabooadmin@yopmail.com",
->>>>>>> delivery
-         subject: "Thank you for your order")
+         subject: "Merci pour votre commande !")
   end
 
   def welcome_admin(order)
-    mail(from: "nans.noel@gmail.com", to: "nabooadmin@yopmail.com",
-         subject: "You have received an order")
+    @order = order
+    mail(from: "nans.noel@gmail.com", to: "nans.noel@gmail.com",
+         subject: "Vous avez reçu une nouvelle commande !")
   end
 
 end
